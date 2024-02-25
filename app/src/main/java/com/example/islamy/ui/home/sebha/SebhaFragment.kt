612af0900ee76081c57d022bbb8689bee26a3a16
ali72-20 <as20201118@gmail.com>
2024-02-25 14:55:52 +0200

    + sebha fragment

--- app/src/main/java/com/example/islamy/ui/home/sebha/SebhaFragment.kt
+++ app/src/main/java/com/example/islamy/ui/home/sebha/SebhaFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.islamy.R
 import com.example.islamy.databinding.FragmentSebhaBinding
 
 class sebhaFragment : Fragment() {
     lateinit var viewBinding : FragmentSebhaBinding
+    var counter : Int = 0
+    var currentZekrindex :Int = 0
+   lateinit var azkarList:MutableList<String>
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,5 +20,25 @@ class sebhaFragment : Fragment() {
     ): View? {
         viewBinding = FragmentSebhaBinding.inflate(layoutInflater,container,false)
         return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+         azkarList = resources.getStringArray(R.array.azkarList).toMutableList()
+        onSebhaClick()
+    }
+
+    private fun onSebhaClick() {
+        viewBinding.sebhaBodyImgV.setOnClickListener{
+            viewBinding.sebhaBodyImgV.rotation += (360/33).toFloat() // float in kotlin
+            if(counter < 33){
+                counter++
+            }else{
+                counter = 0
+                currentZekrindex = if(currentZekrindex  < azkarList.size - 1) ++currentZekrindex else 0
+                viewBinding.zeker.text = azkarList[currentZekrindex]
+            }
+            viewBinding.counter.text = counter.toString()
+        }
     }
 }
